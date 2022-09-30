7cdee0cfd77ed1307451ba9b83733be9b6ee3ab0
hyeongwookimdev <hyeongwookim.dev@gmail.com>
2022-09-30 18:27:28 +0900

    fix routing

--- src/App.js
+++ src/App.js
@@ -10,11 +10,11 @@ const router = createBrowserRouter([
     element: <Home />,
   },
   {
-    path: `${process.env.PUBLIC_URL}/to-do-list`,
+    path: `/to-do-list`,
     element: <ToDoList />,
   },
   {
-    path: `${process.env.PUBLIC_URL}/coin-tracker`,
+    path: `/coin-tracker`,
     element: <CoinTracker />,
   },
 ]);
